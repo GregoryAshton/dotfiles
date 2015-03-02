7285a93ab9608f83486450f41b2cfcd60e959df9
Gregory Ashton <gash789@gmail.com>
2015-03-02 08:57:08 +0000

    Adds check for existing vim config

--- install.sh
+++ install.sh
@@ -1,23 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 # Get current dir (so run this script from anywhere)
-
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update dotfiles itself first
-
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
-# Bunch of symlinks
+# symlinks for standard files
 ln -sfv "$DOTFILES_DIR/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/.vim" ~
 
-# Install Vundle
-echo "Set up vim, Vundle and install plugins"
-cd ~/.vim
-./InstallVundle
-vim +PluginInstall +qall
+# vim setup
+if [ -d ~/.vim ]; then
+  echo "~/.vim already exist - ignoring vim setup"
+else
+  ln -sfv "$DOTFILES_DIR/.vim" ~
 
-# Install Powerline fonts
-./InstallPowerlineFonts
+  # Install Vundle
+  echo "Set up vim, Vundle and install plugins"
+  cd ~/.vim
+  ./InstallVundle
+  vim +PluginInstall +qall
+
+  # Install Powerline fonts
+  ./InstallPowerlineFonts
+fi
+
+
