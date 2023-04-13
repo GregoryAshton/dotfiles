9fbc976c87a5e340ea05ab5a3bb7cae8e283fea1
Gregory Ashton <gregory.ashton@ligo.org>
2023-04-13 10:18:54 +0000

    Modernisze

--- install.sh
+++ install.sh
@@ -14,40 +14,33 @@ export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ -f ~/.bashrc ]; then
     echo "WARNING: ~/.bashrc already exists: no action taken"
 else
-    ln -sfv "$DOTFILES_DIR/.bashrc" ~
+    ln -sfv "$DOTFILES_DIR/bashrc" ~/.bashrc
 fi
 
 if [ -f ~/.gitconfig ]; then
     echo "WARNING: ~/.gitconfig already exists: no action taken"
 else
-    ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+    ln -sfv "$DOTFILES_DIR/gitconfig" ~/.gitconfig
 fi
 
 if [ -f ~/.tmux.conf ]; then
     echo "WARNING: ~/.tmux.conf already exists: no action taken"
 else
-    ln -sfv "$DOTFILES_DIR/.tmux.conf" ~
+    ln -sfv "$DOTFILES_DIR/tmux.conf" ~/.tmux.conf
 fi
 
 if [ -f ~/.dir_colors ]; then
     echo "WARNING: ~/.dir_colors already exists: no action taken"
 else
-    ln -sfv "$DOTFILES_DIR/.dir_colors" ~
+    ln -sfv "$DOTFILES_DIR/dir_colors" ~/.dir_colors
 fi
-
-if [ -f ~/.batchgitrc ]; then
-    echo "WARNING: ~/.batchgitrc already exists: no action taken"
-else
-    ln -sfv "$DOTFILES_DIR/.batchgitrc" ~
-fi
-
 
 
 # vim setup
 if [ -d ~/.vim ]; then
   echo "WARNING: ~/.vim already exist: no action taken"
 else
-  ln -sfv "$DOTFILES_DIR/.vim" ~
+  ln -sfv "$DOTFILES_DIR/vim" ~/.vim
 
   # Install Vundle
   echo "Set up vim, Vundle and install plugins"
@@ -58,31 +51,3 @@ else
   # Install Powerline fonts
   ./InstallPowerlineFonts
 fi
-
-
-# ipython setup
-if ! type "$ipython" > /dev/null; then
-    ipython profile create default # Generate the file structure (if it doesn't exist)
-fi
-if [ -f ~/.ipython/profile_default/startup/ipython_imports.py ]; then
-    echo "WARNING: .ipython/profile_default/startup/ipython_imports.py already exists: no action taken"
-else
-    ln -sfv "$DOTFILES_DIR/ipython_imports.py" ~/.ipython/profile_default/startup/ipython_imports.py
-fi
-
-
-# matplotlib setup
-if [ -f ~/.config/matplotlib/stylelib/paper.mplstyle ]; then
-    echo "WARNING: .config/matplotlib/stylib/paper.mplstyle already exists: no action taken"
-else
-    mkdir -p ~/.config/matplotlib/stylelib
-    ln -sfv "$DOTFILES_DIR/paper.mplstyle" ~/.config/matplotlib/stylelib/paper.mplstyle
-fi
-
-if [ -f ~/.config/matplotlib/stylelib/thesis.mplstyle ]; then
-    echo "WARNING: .config/matplotlib/stylib/thesis.mplstyle already exists: no action taken"
-else
-    mkdir -p ~/.config/matplotlib/stylelib
-    ln -sfv "$DOTFILES_DIR/thesis.mplstyle" ~/.config/matplotlib/stylelib/thesis.mplstyle
-fi
-
